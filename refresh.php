d642f59cbd8d1c0648c6aec0a67d38f59ed1af5a
rtomoham <rtomoham@cisco.com>
2020-06-22 07:46:30 +0200

    Set timezone from TZ ENV
    Default value: Europe/Amsterdam

--- refresh.php
+++ refresh.php
@@ -1,6 +1,10 @@
 <?php
 include('Worker.php');
 
+if (!date_default_timezone_set(getenv('TZ'))) {
+ date_default_timezone_set('Europe/Amsterdam');
+};
+
 $worker = new Worker();
 $worker->refreshGoogleCalendars();
  ?>
