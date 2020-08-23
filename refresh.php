9c2ac8c478d4fa57fb2035d4e19e4a6c9f712696
rtomoham <rtomoham@cisco.com>
2020-08-23 14:18:21 +0200

    Removed timezone config (moved to Utils.php)

--- refresh.php
+++ refresh.php
@@ -1,10 +1,6 @@
 <?php
 include('Worker.php');
 
-if (!date_default_timezone_set(getenv('TZ'))) {
- date_default_timezone_set('Europe/Amsterdam');
-};
-
 $worker = new Worker();
 $worker->refreshGoogleCalendars();
  ?>
